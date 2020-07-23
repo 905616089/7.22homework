69aaa9ba74c18023e8221cdae2532008edfcc25d
905616089 <57530010+905616089@users.noreply.github.com>
2020-07-23 22:42:06 +0800

    打字游戏

--- work.js
+++ work.js
@@ -9,9 +9,7 @@ var box = document.querySelector(".box");
 var btnlist = document.querySelectorAll(".box button")
 var over = 3;
 var value = 0;
-var real=0; //销毁个数
-span2.innerHTML = over;
-span1.innerHTML = value;
+var real = 0; //销毁个数
 var g = 1;//第几关;
 var sum = 10;//当前关方块数量;
 var nowWord = [];
@@ -20,10 +18,9 @@ var speed = 5;
 var num = 3;
 
 function craetword(num, speed) {
+    console.log(num);
     for (var i = 0; i < num; i++) {
-
         nowWord.push(word[Math.floor(word.length * Math.random())])
-
         var wordDiv = document.createElement("div");
         wordDiv.innerHTML = word[Math.floor(word.length * Math.random())];
 
@@ -31,12 +28,12 @@ function craetword(num, speed) {
 
         container.appendChild(wordDiv);
         divs.push(wordDiv);
-       
+
 
     }
     console.log(divs);
     function clearword(speed) {
-        for (var x = 0; x < divs.length; x++) {   
+        for (var x = 0; x < divs.length; x++) {
             if (over < 0 || over == 0) {
                 clearInterval(t);
             }
@@ -48,7 +45,7 @@ function craetword(num, speed) {
                 speed = speed + 0.1;
                 divs.splice(x, 1);
                 clearInterval(t)
-             
+
                 craetword(1, speed);
                 span2.innerHTML = over;
                 console.log(over);
@@ -76,7 +73,7 @@ function craetword(num, speed) {
                     container.removeChild(divs[i]);
                     divs.splice(i, 1);
                     clearInterval(t);
-                    
+
                     speed = speed + 0.1;
                     craetword(1, speed);
                     break;
@@ -101,16 +98,15 @@ function beg(num, speed) {
         x = x - 1;
     }, 1000);
 }
-function levebeg(num, speed, g,sum) {
+function levebeg(num, speed, g, sum) {
     var x = 3;
-  
     black.style.display = "block";
     black.innerHTML = "第" + g + "关";
-    setTimeout(guanka, 4000, num, speed, g,sum);
+    setTimeout(guanka, 4000, num, speed, g, sum);
     var y = setInterval(() => {
         if (x == 0) {
             clearInterval(y);
-            black.style.display = "none";          
+            black.style.display = "none";
         }
         black.innerHTML = x;
         x = x - 1;
@@ -123,7 +119,7 @@ leve.onclick = function () {
 
 for (let h = 0; h < btnlist.length; h++) {
     btnlist[h].onclick = function () {
-        if (h == 0) {
+        if (h == 0) {   
             num = 3;
             speed = 5;
         }
@@ -139,15 +135,11 @@ for (let h = 0; h < btnlist.length; h++) {
             num = "a";
             speed = "a";
         }
-
         box.style.display = "none";
     }
 }
 
-
-
-
-function guanka(num, speed, g,sum) {
+function guanka(num, speed, g, sum) {
     for (var i = 0; i < num; i++) {
         nowWord.push(word[Math.floor(word.length * Math.random())])
         var wordDiv = document.createElement("div");
@@ -156,11 +148,11 @@ function guanka(num, speed, g,sum) {
         wordDiv.style.cssText = "font-size: 30px;font-weight: 900;color: #00AF4F;text-shadow: #009643 1px 1px, #16ff7f -1px -1px, #00c939 -2px -2px 6px, #00d861 -2px -2px, #00d861 -1px -2px, #00d861 -1px -3px, #00d861 -2px -4px, #00d861 -2px -5px, #00d861 -3px -6px, #00AF4F -4px -7px, rgba(0, 0, 5, 0.4) 3px 4px 5px, rgba(0, 0, 5, 0.2) -3px -4px 5px;transform: rotate(-2deg);position:absolute;left:" + container.offsetWidth * Math.random() + "px;top:10px;"
         container.appendChild(wordDiv);
         divs.push(wordDiv);
-       
+
     }
     console.log(divs);
     function clearwd(speed) {
-    
+
         for (var x = 0; x < divs.length; x++) {
             if (over < 0 || over == 0) {
                 clearInterval(t);
@@ -170,9 +162,9 @@ function guanka(num, speed, g,sum) {
                 divs[x].style.display = "none";
                 over = over - 1;
                 divs.splice(x, 1);
-                clearInterval(t);  
-                console.log("sdasd"+sum)
-                guanka(1, speed,g,sum);
+                clearInterval(t);
+                console.log("sdasd" + sum)
+                guanka(1, speed, g, sum);
                 span2.innerHTML = over;
 
                 if (over < 0 || over == 0) {
@@ -181,9 +173,10 @@ function guanka(num, speed, g,sum) {
                     alert("游戏结束,您的得分为" + value);
                     black.innerHTML = "";
                     container.innerHTML = "";
-                    over = 3;
                     value = 0;
-                    divs.splice(0, divs.length);
+                    num=3;
+                    speed=5;
+                    divs.splice(x, 1);
                     span2.innerHTML = over;
                     span1.innerHTML = value;
                     return;
@@ -195,28 +188,28 @@ function guanka(num, speed, g,sum) {
             for (var i = 0; i < divs.length; i++) {
                 if (divs[i].innerHTML == word) {
                     value = value + 1;
-                    real=real+1;
+                    real = real + 1;
                     console.log(sum);
                     if (real == sum || real > sum) {
                         console.log("下关")
                         black.innerHTML = "";
-                        container.innerHTML = "";                 
+                        container.innerHTML = "";
                         span2.innerHTML = over;
                         divs.splice(0, divs.length);
                         //下关数据
                         over = 3;
-                        g=g+1;
-                        sum=sum+5;
-                        speed=speed+0.3;
-                        num=num+g;
-                        real=0;
-                        levebeg(num, speed, g,sum)
-                    } else {  
+                        g = g + 1;
+                        sum = sum + 5;
+                        speed = speed + 0.3;
+                        num = num + g;
+                        real = 0;
+                        levebeg(num, speed, g, sum)
+                    } else {
                         span1.innerHTML = value;
                         container.removeChild(divs[i]);
                         divs.splice(i, 1);
-                        clearInterval(t);  
-                        guanka(1,speed,g,sum);
+                        clearInterval(t);
+                        guanka(1, speed, g, sum);
                         break;
                     }
                 }
@@ -227,23 +220,20 @@ function guanka(num, speed, g,sum) {
 
 }
 
-
-
-
-
-
-
 begin.onclick = function () {
-    over=3;
-    divs.splice(0,divs.length);
-    span2.innerHTML=over;
-   if (isNaN(speed) &&isNaN(num)) {
+    //开始数据初始化
+    over = 3;
+    divs.splice(0, divs.length);
+    span2.innerHTML = over;
+    span1.innerHTML = value;
+    if (isNaN(speed) && isNaN(num)) {
         console.log("关卡")
-        num=2;speed=3;
-        console.log(num, speed,g,sum);
-        levebeg(num, speed, g,sum)
+        num = 2; speed = 3;
+        console.log(num, speed, g, sum);
+        levebeg(num, speed, g, sum)
     } else {
-        console.log("普通")
+        console.log("普通");
+        
         console.log(speed);
         beg(num, speed);
     }
